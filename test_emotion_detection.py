7af826fed27c2befe21a03025483dcdabd56f0af
mertgokcen <mertgursugokcen@gmail.com>
2026-06-28 13:53:19 -0400

    Function name fix

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -3,19 +3,19 @@ import unittest
 
 class TestSentimentAnalyzer(unittest.TestCase): 
     def test_sentiment_analyzer(self):
-        result = sentiment_analyzer('I am glad this happened')
+        result = emotion_detector('I am glad this happened')
         self.assertEqual(result['dominant_emotion'], 'joy')
         
-        result = sentiment_analyzer('I am really mad about this')
+        result = emotion_detector('I am really mad about this')
         self.assertEqual(result_1['dominant_emotion'], 'anger')
         
-        result = sentiment_analyzer('I feel disgusted just hearing about this')
+        result = emotion_detector('I feel disgusted just hearing about this')
         self.assertEqual(result['dominant_emotion'], 'disgust')
         
-        result = sentiment_analyzer('I am so sad about this')
+        result = emotion_detector('I am so sad about this')
         self.assertEqual(result_1['dominant_emotion'], 'sadness')
         
-        result = sentiment_analyzer('I am really afraid that this will happen')
+        result = emotion_detector('I am really afraid that this will happen')
         self.assertEqual(result_1['dominant_emotion'], 'fear')
 
 unittest.main()
